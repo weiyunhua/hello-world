f85eeed93e92b415dcb92713352f65b72b38483b
wyhua <wyhua.521@qq.com>
2020-05-31 18:52:45 +0800

    add screen printing in kernel
    Change-Id: I2e2c55497fcb48691709ce35180ecb564a1f9bcf

---
+++ system/40/DTOS/boot.asm
@@ -0,0 +1,39 @@
+
+
+%include "blfunc.asm"
+%include "common.asm"
+
+org BaseOfBoot
+
+interface:
+    BaseOfStack    equ    BaseOfBoot
+    BaseOfTarget   equ    BaseOfLoader
+    Target db  "LOADER     "
+    TarLen equ ($-Target)
+
+BLMain:
+    mov ax, cs
+    mov ss, ax
+    mov ds, ax
+    mov es, ax
+    mov sp, SPInitValue
+
+    call LoadTarget
+
+    cmp dx, 0
+    jz output
+    jmp BaseOfLoader
+
+output:	
+    mov bp, ErrStr
+    mov cx, ErrLen
+    call Print
+
+    jmp $
+
+ErrStr db  "No LOADER"
+ErrLen equ ($-ErrStr)
+
+Buffer:
+    times 510-($-$$) db 0x00
+    db 0x55, 0xaa
---
+++ system/40/DTOS/kentry.asm
@@ -0,0 +1,17 @@
+
+
+global _start
+
+extern KMain
+extern ClearScreen
+
+[section .text]
+[bits 32]
+_start:
+    mov ebp, 0
+
+    call ClearScreen
+    call KMain
+
+    jmp $
+
